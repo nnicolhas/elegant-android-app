e9118fe87073116215a4433d2e3024573100bc3f
nnicolhas <nicojoel.etchegaray@gmail.com>
2025-08-16 00:24:47 -0300

    Add app/src/main/java/com/elegantapp/ui/theme/Color.kt 1755314686730

---
+++ app/src/main/java/com/elegantapp/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.elegantapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Elegant color palette
+val ElegantBlack = Color(0xFF000000)
+val ElegantWhite = Color(0xFFFFFFFF)
+val ElegantGray = Color(0xFFCCCCCC)
+val ElegantDarkGray = Color(0xFF333333)
+val ElegantAccent = Color(0xFF007AFF)
+val ElegantGold = Color(0xFFFFD700)
